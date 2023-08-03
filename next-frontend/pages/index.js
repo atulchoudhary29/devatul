4ba1bc0804f2bd7c2524b866882ece8cc2e501ad
Atul Choudhary <102431647+atulchoudhary29@users.noreply.github.com>
2023-08-03 15:24:02 +0530

    fixed image url

--- next-frontend/pages/index.js
+++ next-frontend/pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ blogs}) {
     title: "DevAtul",
     name: "Atul",
     fullname: "Atul Choudhary",
-    image: "/assets/img/atul.jpg",
+    image: "./public/assets/img/atul.jpg",
     kaggleLink: "https://www.kaggle.com/atulchoudhary29",
     twitterLink: "https://twitter.com/AtulChoudhary08",
     linkdinLink: "https://www.linkedin.com/in/atulchoudhary2908/",
